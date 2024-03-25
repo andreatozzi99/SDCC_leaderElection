3041b292c7032785b18b2923d6bdd7233e22bca1
andreatozzi99 <andreatozzi199@gmail.com>
2024-03-25 11:11:34 +0100

    9:30 - 11:00
    Avanzamenti su node.go, mancano metodi "STOP" e "COORDINAATOR"

--- nodes/node.go
+++ nodes/node.go
@@ -10,14 +10,43 @@ import (
 )
 
 // Variabile che rappresentano lo stato (variabile) di un nodo
-var nodeList []Node // Lista dei nodi di cui un nodo è a conoscenza
-var leaderID = -1   // ID dell'attuale leader
+var nodeList []Node    // Lista dei nodi di cui un nodo è a conoscenza
+var leaderID = -1      // ID dell'attuale leader
+var leaderAddress = "" // Indirizzo dell'attuale leader, inizialmente sconosciuto
 
 // Node Struttura per rappresentare un nodo
 type Node struct {
 	ID        int
 	IPAddress string
 	Port      int
+}
+
+// ELECTION Metodo per avviare un processo di elezione
+func (n *Node) ELECTION(senderNode Node, reply *bool) error {
+	// Se l'ID del chiamante è più basso di quello del nodo corrente, invoca il metodo "STOP" del chiamante
+	if senderNode.ID < n.ID {
+		// qui sto dando per implicito che tutti gli indirizzi siano "localhost:", ma non va bene
+		client, err := rpc.Dial("tcp", fmt.Sprintf(senderNode.IPAddress, senderNode.Port))
+		if err != nil {
+			fmt.Println("Errore nella connessione con il nodo :", senderNode.ID, ",", senderNode.IPAddress, senderNode.Port)
+			return err
+		}
+		defer func(client *rpc.Client) {
+			err := client.Close()
+			if err != nil {
+				return
+			}
+		}(client)
+		// Invocazione metodo STOP sul nodo
+		var reply bool
+		err = client.Call("Node.Stop", 0, &reply)
+		if err != nil {
+			fmt.Println("Errore durante la chiamata RPC per invocare il metodo 'STOP' sul nodo", senderNode.ID)
+			return err
+		}
+	}
+	// Non fare nulla se l' ID del chiamante è più alto dell' ID locale
+	return nil
 }
 
 // HeartBeat Metodo per contattare il nodo Leader e verificare se esse è ancora attivo
@@ -28,22 +57,24 @@ func (n *Node) HeartBeat(senderID int, reply *bool) error {
 }
 
 func main() {
-	// Avvia un singolo nodo
+	// Creazione struttura nodo
 	node := &Node{
 		ID:        0, // cosi il serverRegistry assegna il giusto ID
 		IPAddress: "localhost",
 	}
+	// Genera una porta casuale disponibile per il nodo
+	node.Port = findAvailablePort()
+
+	// Avvio nodo
 	node.start()
 
-	// Mantieni il programma in esecuzione
+	// Mantiene il programma in esecuzione
 	select {}
 }
 
 // Metodo per avviare il nodo e registrarne l'indirizzo nel server di registrazione
 // e prendere lista dei nodi in rete
 func (n *Node) start() {
-	// Genera una porta casuale disponibile per il nodo
-	n.Port = findAvailablePort()
 
 	// Connette il nodo al server di registrazione
 	client, err := rpc.Dial("tcp", "localhost:8080")
@@ -82,8 +113,12 @@ func (n *Node) start() {
 		fmt.Println("Errore durante la richiesta della lista dei nodi:", err)
 		return
 	}
-
+	// Funzione ausiliaria per stampa del vettore dei nodi (nodi che il serverRegistry conosce)
 	printNodeList()
+
+	// Scoperta dell'attuale leader della rete
+	// -> inizio processo di Elezione, più messaggi sulla rete, ma non richiede ulteriore funzioni
+	n.startElection() // Non è possibile farla in una goRoutine, le operazioni sottostanti dipendono dall'indirizzo del Leader
 
 	// Avvia la goroutine per heartbeat
 	go n.heartBeatRoutine()
@@ -106,7 +141,7 @@ func (n *Node) heartBeatRoutine() {
 	}
 }
 
-// Metodo per contattare il nodo leader
+// Metodo per contattare il nodo leader tramite la chiamata rpc HeartBeat esposta dai nodi
 func (n *Node) isLeaderAlive() bool {
 
 	// Se il nodo stesso è il leader, esci
@@ -115,11 +150,11 @@ func (n *Node) isLeaderAlive() bool {
 	}
 
 	// Una nuova connessione con il nodo leader per ogni ciclo di heartbeat?
-	//########## INDIRIZZO SU FILE CONFIGURAZIONE #################
-	client, err := rpc.Dial("tcp", fmt.Sprintf("localhost:%d", leaderID))
+	client, err := rpc.Dial("tcp", leaderAddress)
 	if err != nil {
 		// Se non è possibile contattare il leader, avvia un processo di elezione
-		fmt.Println("Node", n.ID, "failed to contact leader Node", leaderID)
+		fmt.Println("Node", n.ID, "failed to contact leader Node", leaderID, ":", leaderAddress)
+		// Dovrei contattare il serverRegistry per notificare il crash?
 		return false
 	}
 	defer func(client *rpc.Client) {
@@ -129,25 +164,49 @@ func (n *Node) isLeaderAlive() bool {
 		}
 	}(client)
 
-	// Chiamata RPC per il heartbeat al nodo leader
+	// Chiamata RPC HeartBeat per il meccanismo di failure detection al nodo leader
 	var reply bool
 	err = client.Call("Node.HeartBeat", n.ID, &reply)
 	if err != nil {
 		// Se il leader non risponde, avvia un processo di elezione
 		fmt.Println("Node", n.ID, "failed to contact leader Node", leaderID)
+		// Dovrei contattare il serverRegistry per notificare il crash?
 		return false
 	}
 	//chiamata andata a buon fine
 	return true
 }
 
-// Metodo per avviare un processo di elezione seguendo algoritmo BULLY
+// Metodo per avviare un processo di elezione,
+// e invocare la chiamata rpc ELECTION solo sui nodi nella rete con ID superiore all' ID locale
 func (n *Node) startElection() {
-	// Devo chiamare la procedura remota esposta da ogni nodo nella rete
-	// Nome della procedura: ELECTION (come messaggio dell'algoritmo bully)
 
-	// Dopo aver completato l'elezione, notifica gli altri nodi
-	// n.NotifyLeader()
+	for _, node := range nodeList {
+		if node.ID > n.ID {
+			// Avviata go routine per contattare il nodo
+			go func(nodeID int) {
+				// Effettua una chiamata RPC al nodo specificato per avviare un processo di elezione
+				client, err := rpc.Dial("tcp", fmt.Sprintf(node.IPAddress, node.Port))
+				if err != nil {
+					fmt.Println("Errore nella chiamata RPC per avviare l'elezione sul nodo", node.Port)
+					return
+				}
+				defer func(client *rpc.Client) {
+					err := client.Close()
+					if err != nil {
+						return
+					}
+				}(client)
+
+				var reply bool
+				err = client.Call("Node.Election", n.ID, &reply)
+				if err != nil {
+					fmt.Println("Errore durante la chiamata RPC per avviare l'elezione sul nodo", nodeID)
+					return
+				}
+			}(node.ID)
+		}
+	}
 }
 
 // Funzione per trovare una porta disponibile casuale
